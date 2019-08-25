f676d6ae1e6b2047b871b0f898b07ac8c12b42af
Kittipong Puankunta <proSbeginner@hotmail.com>
2019-08-25 21:18:50 +0700

    show how to use aggregation and lookup operators

--- src/main/java/com/op/security/UserDetailsServiceImpl.java
+++ src/main/java/com/op/security/UserDetailsServiceImpl.java
@@ -1,11 +1,16 @@
 package com.op.security;
 
-import com.op.permission.Permission;
 import com.op.permission.PermissionRepository;
 import com.op.user.Role;
 import com.op.user.User;
 import com.op.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.LookupOperation;
+import org.springframework.data.mongodb.core.aggregation.MatchOperation;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,6 +25,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private UserRepository userRepository;
     @Autowired
     private PermissionRepository permissionRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -32,7 +39,14 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         UserBuilder builder = withUsername(user.getEmail());
         builder.password(user.getPassword());
 
-        List<String> authorities = new ArrayList();
+        builder.authorities(getAuthoritiesByJava(user));
+//        builder.authorities(getAuthoritiesByMongoTeamplate(user));
+
+        return builder.build();
+    }
+
+    private String[] getAuthoritiesByJava(User user) {
+        List<String> authorities = new ArrayList<>();
         permissionRepository.findAll().forEach(permission -> {
             Set<Role> roles = Optional.ofNullable(permission.getRoles()).orElse(new HashSet<>());
             for (Role role : roles) {
@@ -43,8 +57,47 @@ public class UserDetailsServiceImpl implements UserDetailsService {
             }
         });
 
-        builder.authorities(authorities.toArray(new String[authorities.size()]));
+        return authorities.toArray(new String[authorities.size()]);
+    }
 
-        return builder.build();
+    private String[] getAuthoritiesByMongoTeamplate(User user) {
+        MatchOperation matchOperation = Aggregation.match(new Criteria("email").is(user.getEmail()));
+
+        LookupOperation lookupOperation = LookupOperation.newLookup()
+                .from("permissions")
+                .localField("role.name")
+                .foreignField("roles.name")
+                .as("authorities");
+
+        Aggregation aggregation = Aggregation.newAggregation(matchOperation, lookupOperation);
+
+        List<AuthorityResult> authorityResults = mongoTemplate.aggregate(aggregation, User.class, AuthorityResult.class).getMappedResults();
+        AuthorityResult authorityResult = authorityResults.get(0);
+
+        return authorityResult.getAuthorities().stream().map(a -> a.getName()).toArray(String[]::new);
+    }
+}
+
+class AuthorityResult {
+    private List<Authority> authorities;
+
+    public List<Authority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(List<Authority> authorities) {
+        this.authorities = authorities;
+    }
+}
+
+class  Authority {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
