843c57fd46d0455e9c6812e09b86b80174609f9d
Kittipong <prosbeginner@hotmail.com>
2019-10-14 01:14:46 +0700

    remove code not use

--- src/main/java/com/op/file/FileController.java
+++ src/main/java/com/op/file/FileController.java
@@ -1,24 +1,17 @@
 package com.op.file;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
 import com.op.constant.Api;
 import com.op.response.FileResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.data.mongodb.gridfs.GridFsOperations;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 /*
 ref:
@@ -31,36 +24,7 @@ public class FileController {
     private static Log LOG = LogFactory.getLog(FileController.class);
 
     @Autowired
-    private GridFsOperations gridFsOperations;
-
-    @Autowired
     private StorageService storageService;
-
-    String fileId = "";
-
-    @GetMapping("/savefiles")
-    public String saveFile() throws FileNotFoundException {
-        DBObject metadata = new BasicDBObject();
-        metadata.put("organization", "java Techie");
-        InputStream inputStream = new FileInputStream("/Users/nuvola/Downloads/pupe.jpg");
-
-        metadata.put("type", "image");
-        fileId = gridFsOperations.store(inputStream, "pupe.jpg", "Download/jpg", metadata).toString();
-        System.out.println("File in store : " + fileId);
-
-//        metadata.put("type", "data");
-//        gridFsOperations.store(new FileInputStream(""), "mytext.text", "text/pain", metadata);
-
-        return "File store Success";
-    }
-
-//    public String retriveImage() throws IOException {
-//        GridFSDBFile dbFile = gridFsOperations.findOne(new Query(Criteria.where("_id").is(fileId)));
-//        dbFile.writeTo("C:/Users/bahota/Desktop/Local-Disk/reactive-logo.png");
-//        System.out.println("File name : " + dbFile.getFilename());
-//        return "Image File retrived with name : " + dbFile.getFilename();
-//
-//    }
 
     @PreAuthorize("hasAuthority('UPLOAD_FILE')")
     @PostMapping("/file/upload")
