ba62ec2e1e3c14b0268897bb32243311069b55fc
Kittipong <prosbeginner@hotmail.com>
2020-01-26 16:54:11 +0700

    remove not use

--- src/main/java/com/op/permission/PermisionController.java
+++ src/main/java/com/op/permission/PermisionController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.xml.ws.Response;
 import java.util.List;
 import java.util.Optional;
 
