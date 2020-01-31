f8ea199ef65df0cdb7281bfe336d65b68f76bbc4
Kittipong <prosbeginner@hotmail.com>
2020-01-31 15:22:31 +0700

    try to push @CrossOrigin on UserController

--- src/main/java/com/op/user/UserController.java
+++ src/main/java/com/op/user/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin
 @RequestMapping(Api.v1)
 @RestController()
 public class UserController {
