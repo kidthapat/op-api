220183675ddd8dfa1978044a3540a5d0673d2d70
Kittipong <prosbeginner@hotmail.com>
2020-02-17 02:49:21 +0700

    try to report unsuccess login

--- src/main/java/com/op/security/CustomAbstractAuthenticationProcessingFilter.java
+++ src/main/java/com/op/security/CustomAbstractAuthenticationProcessingFilter.java
@@ -1,9 +1,12 @@
 package com.op.security;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.op.request.LoginRequest;
 import com.op.user.User;
 import com.op.user.UserService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,6 +16,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.util.UrlPathHelper;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -24,7 +28,10 @@ import java.util.Collections;
 import java.util.Optional;
 
 public class CustomAbstractAuthenticationProcessingFilter extends AbstractAuthenticationProcessingFilter {
+    private final static Log LOG = LogFactory.getLog(CustomAbstractAuthenticationProcessingFilter.class);
+
     private UserService userService;
+    private final static UrlPathHelper urlPathHeader = new UrlPathHelper();
 
     protected CustomAbstractAuthenticationProcessingFilter(
             String url,
@@ -47,12 +54,13 @@ public class CustomAbstractAuthenticationProcessingFilter extends AbstractAuthen
                         Collections.emptyList()
                 )
         );
-
         return authentication;
     }
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        LOG.info("Logged-in Successful With User " + ((UserDetails) authResult.getPrincipal()).getUsername());
+
         SecurityContextHolder.getContext().setAuthentication(authResult);
 
         String email = ((UserDetails) authResult.getPrincipal()).getUsername();
@@ -65,5 +73,12 @@ public class CustomAbstractAuthenticationProcessingFilter extends AbstractAuthen
         } else {
             writer.print(new ObjectMapper().writeValueAsString(authResult.getPrincipal()));
         }
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        LOG.info("Invalid Password While Attempting To Access " + urlPathHeader.getPathWithinApplication(request));
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid Password");
     }
 }
